22ef5bd92e7433eb86cc5c9aa78071e9f36cd868
Adam Urbina <adamurbina@gmail.com>
2019-01-24 15:39:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function logDriversByHometown(drivers, hometown) {
 }
 
 function driversByRevenue(drivers) {
-  drivers.sort(function (elem1, elem2){
+  return drivers.sort(function (elem1, elem2){
     return elem1.revenue - elem2.revenue
   });
 }
